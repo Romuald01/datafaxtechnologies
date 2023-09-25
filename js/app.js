5f0a1c610ab1002500608c770a3d7a3e12beb475
Romuald01 <romualdadjimagbe2018@gmail.com>
2023-09-25 09:12:09 +0100

    worked on the responsiveness and hamburger functionality

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 "use strict";
 const hamburger = document.querySelector(".hamburger");
+const closeBtn = document.querySelector(".close-button");
 const menu = document.querySelector(".menu-list");
-const overlayMenu = document.querySelector(".menu-list-overlay");
+
+menu.classList.add("hidden");
 
 hamburger.addEventListener("click", function () {
+  menu.classList.remove("hidden");
+
+  document.getElementById("menu").style.width = "100%";
+  document.body.classList.add("no-scroll");
+});
+
+closeBtn.addEventListener("click", function () {
   menu.classList.add("hidden");
-  overlayMenu.classList.add("hidden");
+
+  document.getElementById("menu").style.width = "0%";
 });
