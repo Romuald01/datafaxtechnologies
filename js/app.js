b7a8ea516cea982732ff6593c1b94e5312eb8103
Romuald01 <romualdadjimagbe2018@gmail.com>
2023-10-16 17:58:15 +0100

    updating datafax website

--- js/app.js
+++ js/app.js
@@ -2,9 +2,25 @@
 const hamburger = document.querySelector(".hamburger");
 const closeBtn = document.querySelector(".close-hamburger");
 const menu = document.querySelector(".menu-list");
-const exp = document.getElementById(".experience-div");
+const abilityText = document.querySelector(".ability_text");
 const hid = document.querySelector(".hide-paragraph");
 const paragraph = document.querySelector(".paragraph-text");
+const abilityBtn = document.querySelectorAll(".ability-text-content");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+modal.classList.add("hidden");
+
+for (let i = 0; i < abilityBtn.length; i++)
+  abilityBtn[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+
+closeModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
 
 hamburger.addEventListener("click", function () {
   menu.classList.remove("hidden");
@@ -13,3 +29,7 @@ hamburger.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   menu.classList.add("hidden");
 });
+
+// function heading() {
+
+// }
