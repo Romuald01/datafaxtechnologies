81e859407b35aab684ebf8fd52ee0bab86bd5537
Romuald01 <romualdadjimagbe2018@gmail.com>
2024-06-16 11:02:26 +0100

    updating datafax website

--- js/app.js
+++ js/app.js
@@ -1,36 +1,56 @@
 "use strict";
 const hamburger = document.querySelector(".hamburger");
-const closeBtn = document.querySelector(".close-hamburger");
+// const closeBtn = document.querySelector(".close-hamburger");
+const closeMenu = document.querySelector(".close-hamburger");
 const menu = document.querySelector(".menu-list");
-const abilityText = document.querySelector(".ability_text");
+const abilityText = document.querySelector(".ability-text");
 const hid = document.querySelector(".hide-paragraph");
 const paragraph = document.querySelector(".paragraph-text");
-const abilityBtn = document.querySelectorAll(".ability-text-content");
+const abilityBtn = document.querySelectorAll(".modal-button");
+const experienceBtn = document.querySelectorAll(".exp-modal-button");
 const modal = document.querySelector(".modal");
-const closeModal = document.querySelector(".close-modal");
+const experienceModal = document.querySelector(".expmodal");
+const closeAbilityModal = document.querySelectorAll(".close-modal");
+const closeExperienceModal = document.querySelectorAll(".close-modal");
 const overlay = document.querySelector(".overlay");
-modal.classList.add("hidden");
-// menu.classList.add("hidden");
+closeMenu.classList.add("hidden");
+menu.classList.add("hidden");
+hamburger.addEventListener("click", function (e) {
+  e.preventDefault();
+  menu.classList.remove("hidden");
+  closeMenu.classList.remove("hidden");
+});
 
+closeMenu.addEventListener("click", function (e) {
+  e.preventDefault();
+  menu.classList.add("hidden");
+});
+modal.classList.add("hidden");
+experienceModal.classList.add("hidden");
+
+//display ability modal
 for (let i = 0; i < abilityBtn.length; i++)
   abilityBtn[i].addEventListener("click", function () {
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
   });
+//display experience modal
+for (let i = 0; i < experienceBtn.length; i++)
+  experienceBtn[i].addEventListener("click", function () {
+    experienceModal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
 
-closeModal.addEventListener("click", function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+for (let i = 0; i < closeAbilityModal.length; i++) {
+  closeAbilityModal[i].addEventListener("click", function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  });
+}
 
-hamburger.addEventListener("click", function () {
-  menu.classList.remove("hidden");
-});
-
-closeBtn.addEventListener("click", function () {
-  menu.classList.add("hidden");
-});
-
-// function heading() {
-
-// }
+for (let i = 0; i < closeExperienceModal.length; i++) {
+  closeExperienceModal[i].addEventListener("click", function () {
+    experienceModal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  });
+}
